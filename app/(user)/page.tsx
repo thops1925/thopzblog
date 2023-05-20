a3bfc60b9263df0452839a1c7dd6f0f88b7a3143
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-05-20 15:41:09 +0800

    add revalidate

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,6 +2,8 @@ import BlogList from '@components/BlogList';
 import { client } from '@sanity/lib/client';
 import { author } from '@utils/queries';
 
+export const revalidate = 10; // revalidate this page every 60 seconds
+
 const Home = async () => {
 	const posts = await client.fetch(author());
 	return (
